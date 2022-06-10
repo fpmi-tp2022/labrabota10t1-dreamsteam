894ebc6aa22e35cd769613e1f12bf510422da2a4
Pavel Sudnik <macos@Pavels-MacBook-Pro.local>
2022-06-10 16:50:11 +0300

    design sketch

---
+++ Busik/Busik/SearchViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Busik
+//
+//  Created by Kanstantin Venger on 5/31/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var timetable: UITableView!
+    @IBOutlet weak var cityFromControl: UISegmentedControl!
+    @IBOutlet weak var cityToControl: UISegmentedControl!
+    
+    public var CtxManager: ContextManager!;
+    var _userRepository: UserRepository!;
+    var _localityRepository: LocalityRepository!;
+    var _roureRepository: RouteRepository!;
+    var _ridesRepository: RideRepository!;
+    var _bookedTicketRepository: BookedTicketRepository!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let ctx = ContextRetriever.RetrieveContext();
+        CtxManager = ContextManager(context: ctx);
+        _userRepository = UserRepository(contextManager: CtxManager);
+        _localityRepository = LocalityRepository(contextManager: CtxManager);
+        _roureRepository = RouteRepository(contextManager: CtxManager);
+        _ridesRepository = RideRepository(contextManager: CtxManager);
+        _bookedTicketRepository = BookedTicketRepository(contextManager: CtxManager);
+        
+        let dataSeeder = DataSeeder(ctxManager: CtxManager);
+        
+        //ATTENTION: uncomment to get add test data to db if needed
+        //But do it only once on each device
+        dataSeeder.SeedUsers()
+        dataSeeder.SeedLocalities();
+        dataSeeder.SeedRoutes();
+        dataSeeder.SeedRides();
+        dataSeeder.SeedBookedTickets();
+        
+        //_ridesRepository.GetRides(before: <#T##Date#>, after: <#T##Date#>)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell");
+
+        print("IndexPath: \(indexPath), number \(indexPath.item)\n")
+        return cell!;
+    }
+    
+    
+}
